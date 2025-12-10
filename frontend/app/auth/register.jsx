28948d7cc52af6c642063973058457e2dfed6f2a
bezaadefris1212 <bezaadefris06@gmail.com>
2025-12-09 17:05:04 -0800

    this is the first commit

--- frontend/app/auth/register.jsx
+++ frontend/app/auth/register.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const register = () => {
   return (
     <View>
-      <Text>this is register screen</Text>
+      <Text>register page developed by beza</Text>
     </View>
   )
 }
